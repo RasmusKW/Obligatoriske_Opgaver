06b896262a9283e38ba58b009dc77ef80888abdf
emilnorsker <emilgraaenorsker@gmail.com>
2020-12-19 15:18:54 +0100

    commit

--- src/main/java/com/obl6/Main.java
+++ src/main/java/com/obl6/Main.java
@@ -21,21 +21,44 @@ public class Main
 
         }
 
-
-    static void bubbleSort(int[] arr) {
-        int n = arr.length;
+    /**
+     * Using this algorithm, larger values get pushed to the end of the array faster than smaller values.
+     * Similarly, under water, larger bubbles rise to the top faster than smaller bubbles.
+     * So, it’s fitting that this algorithm would be called Bubble Sort!
+     * @param array
+     */
+    static void bubbleSort(int[] array) {
+        int n = array.length;
         int temp = 0;
-        for(int i=0; i < n; i++){
-            for(int j=1; j < (n-i); j++){
-                if(arr[j-1] > arr[j]){
+        for(int i=0; i < n; i++)
+        {
+            for(int j=1; j < (n-i); j++)
+            {
+                if(array[j-1] > array[j])
+                {
                     //swap elements
-                    temp = arr[j-1];
-                    arr[j-1] = arr[j];
-                    arr[j] = temp;
+                    temp = array[j-1];
+                    array[j-1] = array[j];
+                    array[j] = temp;
                 }
-
             }
         }
-
     }
+
+    /**Big O analysis
+     * three for loops
+     *
+     * Best:
+     * big-O = n
+     * for a already sorted arry, the bubble sort will jut iterate over the array once.
+     *
+     * Average
+     * big-O = n²
+     * for randomly sorted array, bubble sort will iterate the items in a loop (n) an a nested loop(n²) giving the formular : n+n²
+     * which gives n², because of the mechanics og big-O analysis.
+     *
+     * Worst
+     * Big-O = n²
+     * same reason as above with average.
+     */
 }
